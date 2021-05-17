5999de7c988796ac9c97057917cfad78a27d49f1
cr7adi <marampallyaditya42@gmail.com>
2021-05-17 05:02:59 -0400

    deleted task3

--- task3/3.py
+++
@@ -1,8 +0,0 @@
-#3. Write a program to get the sum and multiply of all the items in a given list.
-l = [1,2,3,4,5]
-sum=0
-multiply=1
-for i in range(len(l)):
-    sum+=l[i]
-    multiply*=l[i]
-print("sum=",sum,"Multiply=",multiply)
